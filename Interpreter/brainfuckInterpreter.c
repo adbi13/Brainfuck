7bca5b1e647124d315c079fc56ad9b9ff9203ed3
adbi13 <adela.bierska@gmail.com>
2022-01-09 20:02:09 +0100

    Finished Brainfuck Interpreter

--- Interpreter/brainfuckInterpreter.c
+++ Interpreter/brainfuckInterpreter.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "brainfuckLib.h"
 
 #define VALID 0
 #define MISSING_LEFT 1
 #define MISSING_RIGHT 2
 #define ENDLESS_LOOP 3
-
-struct Program
-{
-    char *commands;
-    size_t commandsSize;
-    size_t actualCommand;
-
-    unsigned char *memory;
-    size_t memorySize;
-    unsigned char *pointerPosition;
-};
 
 
 int checkRightBracket(int *bracketStack, bool emptyLoop,
@@ -94,30 +84,44 @@ int validate(FILE* input)
     return VALID;
 }
 
-
-int run(FILE *input)
+int runProgram(FILE *input)
 {
     struct Program program;
-    
-}
 
+    if (initProgram(&program) != BF_SUCCESS)
+    {
+        return BF_FAILURE;
+    }
+
+    if (load(input, &program) != BF_SUCCESS)
+    {
+        destroyProgram(&program);
+        return BF_FAILURE;
+    }
+
+    if (run(&program) != BF_SUCCESS)
+    {
+        destroyProgram(&program);
+        return BF_FAILURE;        
+    }
+
+    destroyProgram(&program);
+    return BF_SUCCESS;
+}
 
 int main(int argc, char *argv[])
 {
-    FILE *input;
-
-    if (argc == 0)
+    if (argc == 1)
     {
-        input = stdin;
+        fprintf(stderr, "Error: Expected file to open.\n", argv[0]);
+        return EXIT_FAILURE;
     }
-    else
+
+    FILE *input = fopen(argv[0], "r");
+    if (!input)
     {
-        input = fopen(argv[0], "r");
-        if (!input)
-        {
-            fprintf(stderr, "Error: Can't open file %s.\n", argv[0]);
-            return EXIT_FAILURE;
-        }
+        fprintf(stderr, "Error: Can't open file %s.\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     if (validate(input) != VALID)
@@ -126,5 +130,19 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    if (fseek(input, 0, SEEK_SET) != 0)
+    {
+        fprintf(stderr, "Error: Problem occured while reading from %s.\n", argv[0]);
+        fclose(input);
+        return EXIT_FAILURE;        
+    }
 
+    if (runProgram(input) != BF_SUCCESS)
+    {
+        fclose(input);
+        return EXIT_FAILURE;        
+    }
+
+    fclose(input);
+    return EXIT_SUCCESS;
 }
